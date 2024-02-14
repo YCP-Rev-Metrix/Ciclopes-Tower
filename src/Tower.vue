b8a46fb9f8c5c52d94c9b98de930b210e58a2d59
thbloomquist <tbloomquist@ycp.edu>
2024-02-14 12:07:34 -0500

    First commit

---
+++ src/Tower.vue
@@ -0,0 +1,32 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <!--<header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+
+    
+  </main>
+  -->
+  <main>
+  <div style="display:flex;justify-content:center;align-items:center;">
+    <div style="width:800px;height:500px;background-color:sandybrown;">
+      <img alt="Vue logo" class="logo" src="./assets/Bowling-Lane.svg" width="800px" height="500px"/>
+    </div>
+  </div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+//import App from './App.vue'
+import App from './Tower.vue'
+
+createApp(App).mount('#app')
